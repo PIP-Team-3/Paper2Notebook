e7252141ed10197a50744992dbdda54a69059bf4
Justin Shieh <27078349+jtsshieh@users.noreply.github.com>
2025-12-08 15:30:34 -0500

    fix: storybook supabase key env variable

--- storybookGenerator/download_paper.py
+++ storybookGenerator/download_paper.py
@@ -5,7 +5,7 @@ import os
 # Load .env
 load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
